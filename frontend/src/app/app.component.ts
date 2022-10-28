97f34774e4bc9a5231785bd42d0b4863ebd927e5
FrederikReigstad <Frederik9876@gmail.com>
2022-10-28 21:21:54 +0200

    made life improments

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,67 +6,64 @@ import {HttpService} from "../services/http.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit{
-  BoxSize: string = "";
-  CustomerName: string = "";
-  Type: string = "";
+  formModel : Box = new Box();
   box: any;
-
-
+  boxes: Array<Box> = [];
 
 
   constructor(private http : HttpService) {
 
   }
 
-
- async ngOnInit(){
+  async ngOnInit(){
     const boxData = await this.http.getBox();
+    this.boxes = boxData;
     this.box = boxData;
   }
 
-  async createBox(){
-    let dto = {
-      size: this.BoxSize,
-      customerName: this.CustomerName,
-      type: this.Type,
+  async postForm(){
+    if(this.formModel.id === 0){
+      await this.createBox();
+    } else {
+      await this.editBox(this.formModel.id);
     }
-    const result = await this.http.createBox(dto);
-    this.box.push(result);
-    console.log(result.data);
-    this.BoxSize = "";
-    this.CustomerName = "";
-    this.Type = "";
   }
 
-  writeBoxSize(){
-    console.log(this.BoxSize);
-    console.log(this.CustomerName);
-    console.log(this.Type);
+  async createBox(){
+    const result = await this.http.createBox(this.formModel as BoxDto);
+    this.boxes.push(result);
+    this.formModel = new Box();
   }
-
 
   async deleteBox(id:any) {
    const box = await this.http.deleteBox(id);
-   this.box = this.box.filter((b: { id: any; }) => b.id != box.id)
+   this.boxes = this.boxes.filter((b: { id: any; }) => b.id != box.id)
   }
 
   async editBox(id: any) {
-    console.log(this.BoxSize);
-    console.log(this.CustomerName);
-    console.log(this.Type);
-    let dto = {
-      id: id,
-      size: this.BoxSize,
-      customerName: this.CustomerName,
-      type: this.Type,
-    }
-    const box = await this.http.editBox(id, dto);
-    console.log("This is box", box);
-    this.box = this.box.filter((b: { id: any; }) => b.id != box.id)
-    this.box.push(box);
-    this.BoxSize = "";
-    this.CustomerName = "";
-    this.Type = "";
+    const box = await this.http.editBox(id, this.formModel);
+    let indexToEdit = this.boxes.findIndex(b => b.id == id);
+    this.boxes[indexToEdit] = box;
+    this.formModel = new Box();
   }
+
+  selectCard(box : Box){
+    this.formModel = {...box}; // creates a copy of box and sets it to the form - done to prevent 2 way binding
+  }
+
+  clearForm(){
+    this.formModel = new Box();
+  }
+}
+
+class BoxDto {
+  size: string = "";
+  customerName: string = "";
+  type: string = "";
+}
+
+class Box extends BoxDto{
+  id: number = 0;
 }
